23777432594bef57ab781196fd01f6eeab593919
Freakyy <jan.schmitter@gmail.com>
2015-04-27 16:54:59 +0200

    Forgot to update test_script

--- build/GTALua/addons/test_script/main.lua
+++ build/GTALua/addons/test_script/main.lua
@@ -1,24 +1,23 @@
 -- test_script
 thread = ScriptThread("test_script")
 
+-- Run
 function thread:Run()
-	print("Lua: thread:Run()")
+	print("test_script:Run")
 	
 	while self:IsActive() do
-		if IsKeyDown(0x78) then
-			print("ok!")
+		-- Spawn Taxi
+		if IsKeyDown(0x78) then -- F9
+			-- Note: Taxi model is always loaded, no need to load it first
 			local localplayer = natives.PLAYER.GET_PLAYER_PED(-1)
-			print("lplayer: ", localplayer)
-			
 			local vec = natives.ENTITY.GET_ENTITY_COORDS(localplayer, true)
-			print("pos: ", vec.x, vec.y, vec.z)
-			
-			local r = natives.VEHICLE.CREATE_VEHICLE(0xC703DB5F, vec.x, vec.y, vec.z, 0.0, true, true)
-			print("r: ", r)
+			natives.VEHICLE.CREATE_VEHICLE(0xC703DB5F, vec.x, vec.y, vec.z, 0.0, true, true)
 		end
 		
+		-- Wait
 		self:Wait(50)
 	end
 end
 
+-- Register Thread
 thread:Register()
