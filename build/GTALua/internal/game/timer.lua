4a9df3145ca43ea2cc4a9e1c3c2a91673559ff28
Freakyy <jan.schmitter@gmail.com>
2015-05-11 23:00:14 +0200

    Timers: Fixed variable arguments for timer module

--- build/GTALua/internal/game/timer.lua
+++ build/GTALua/internal/game/timer.lua
@@ -4,21 +4,21 @@ timer = {}
 
 -- Simple
 -- Runs once, cannot be cancelled!
-function timer.Simple(ms, callback)
-	return timer.Create(nil, ms, 1, callback)
+function timer.Simple(ms, callback, ...)
+	return timer.Create(nil, ms, 1, callback, unpack({...}))
 end
 
 -- Create
 -- Make sure name is unique
 -- runs = -1 => unlimited runs
-function timer.Create(name, ms, runs, callback)
+function timer.Create(name, ms, runs, callback, ...)
 	-- Thread
 	if scripthookv.ActiveThread == nil then
 		error("timer.Simple failed! Unknown active thread! Has this been called outside of a thread?")
 	end
 	
 	-- Add
-	return scripthookv.ActiveThread:AddTimer(name, ms, runs, callback)
+	return scripthookv.ActiveThread:AddTimer(name, ms, runs, callback, unpack({...}))
 end
 
 -- Cancel
