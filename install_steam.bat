615c774b58c5776c39aa00dc6212ad6a6ee2def3
MockbaTheBorg <mockba.the.borg@gmail.com>
2016-01-29 08:50:01 -0500

    Corrected the install_steam.bat file
    Corrected the install_steam.bat file so it will create the GTALua folder
    under GTA V if it doesn't exist.

--- install_steam.bat
+++ install_steam.bat
@@ -1,4 +1,5 @@
 @echo off
 echo "Installing Steam files"
+mkdir "C:\Program Files (x86)\Steam\steamapps\common\Grand Theft Auto V\GTALua"
 for %%a in ("build\GTALua.asi" "build\*.dll") do (xcopy /s /q /y "%%~a" "C:\Program Files (x86)\Steam\steamapps\common\Grand Theft Auto V\")
-for %%a in ("build\GTALua") do (xcopy /s /q /y "%%~a" "C:\Program Files (x86)\Steam\steamapps\common\Grand Theft Auto V\GTALua")
+for %%a in ("build\GTALua") do (xcopy /s /q /y "%%~a" "C:\Program Files (x86)\Steam\steamapps\common\Grand Theft Auto V\GTALua\")
